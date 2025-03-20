ef75603ad8c4c2cdcc72d56d9625e8e77a4413dd
Exhelsior <alejandrosilva.82@hotmail.com>
2025-03-19 23:01:18 -0500

    autenticacion

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express'
 import { validationResult } from 'express-validator'
 import slug from 'slug'
 import User from "../models/User"
-import { hashPassword } from '../utils/auths'
+import { checkPassword, hashPassword } from '../utils/auths'
 
 export const createAccount = async (req: Request, res: Response) => {
     
@@ -18,7 +18,7 @@ export const createAccount = async (req: Request, res: Response) => {
     const userExist = await User.findOne({ email })
     if (userExist) {
         const error = new Error('Un usuario con ese email ya esta registrado')
-        res.status(489).json({ error: error.message }) 
+        res.status(409).json({ error: error.message }) 
         return
     }
 
@@ -26,7 +26,7 @@ export const createAccount = async (req: Request, res: Response) => {
     const handleExist = await User.findOne({ handle })
     if (handleExist) {
         const error = new Error('Nombre de usuario no disponible')
-        res.status(489).json({ error: error.message })
+        res.status(409).json({ error: error.message })
         return
     }
 
@@ -37,4 +37,31 @@ export const createAccount = async (req: Request, res: Response) => {
 
     await user.save()
     res.status(201).send('Registro Creado Correactamente')
+}
+
+export const login = async (req: Request, res: Response) => {
+    let errors = validationResult(req)
+    if (!errors.isEmpty()){
+        res.status(400).json({ errors: errors.array() })
+        return
+    }
+
+    const { email, password } = req.body
+    //Revizar si el usuario existe
+    const user = await User.findOne({ email })
+    if (!user) {
+        const error = new Error('El usuario no existe')
+        res.status(404).json({ error: error.message }) 
+        return
+    }
+
+    //Comprobar el password
+    const isPasswordCorrect = await checkPassword(password, user.password)
+    if (!isPasswordCorrect) {
+        const error = new Error('Password incorrecto')
+        res.status(404).json({ error: error.message }) 
+        return
+    }
+
+    res.send('Autenticando...')
 }
--- src/router.ts
+++ src/router.ts
@@ -1,13 +1,34 @@
 import { Router } from "express";
 import { body } from "express-validator";
-import { createAccount } from "./handlers";
+import { createAccount, login } from "./handlers";
 
 
 const router = Router()
 
 router.post('/auth/register',
-    body('handle').notEmpty(),
+    body('handle')
+        .notEmpty()
+        .withMessage('El handle no puede ir vacio'),
+    body('name')
+        .notEmpty()
+        .withMessage('El nombre no puede ir vacio'),
+    body('email')
+        .isEmail()
+        .withMessage('E-mail no valido'),
+    body('password')
+        .isLength({ min: 8})
+        .withMessage('El password es muy corto. Minimo 8 caracteres'),
     createAccount)
+
+router.post('/auth/login',
+    body('email')
+        .isEmail()
+        .withMessage('E-mail no valido'),
+    body('password')
+        .notEmpty()
+        .withMessage('El password es obligatorio'),    
+    login
+);
 
 
 export default router
